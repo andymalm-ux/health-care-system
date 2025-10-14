6f6f1470ef9191367ff9755ae8103f432d09c8e6
theazukanovic <thea.zukanovic@student.nbi-handelsakademin.se>
2025-10-14 14:55:59 +0200

    Added enum for menu and structure menu logic
    So that the menu is easier to read and understand.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using App;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics;
+using App;
 
 List<User> users = new List<User>();
-User? activeUser = null;//startar programmet utan ett inloggat konto
+User? activeUser = null; //startar programmet utan ett inloggat konto
+Menu menu = Menu.None;
 
 bool running = true;
 
@@ -11,52 +12,100 @@ users.Add(new User("e", "a"));
 
 while (running)
 {
-  if (activeUser == null)//ingen användare inloggad, inlogg meny
-  {
-    Console.WriteLine("---Welcome to health care system---\n1. Log in");
-    string? input = Console.ReadLine();
-    
-    switch (input)
+    try
     {
-      case "1":
-        try { Console.Clear(); } catch { }
-        Console.WriteLine("Enter Email: ");
-        string? email = Console.ReadLine();
-        try { Console.Clear(); } catch { }
-
-        Console.WriteLine("Enter password: ");
-        string? password = Console.ReadLine();
-        try { Console.Clear(); } catch { }
-
-        Debug.Assert(email != null);
-        Debug.Assert(password != null);
-
-        if(email == "" && password == "")
-                {
-          Console.WriteLine("Enter valid input");
-                }
-
-        foreach (User user in users)
-        {
-          if (user.TryLogin(email, password))
-          {
-            activeUser = user;
-            break;
-          }
-          else
-            {
-            Console.WriteLine("Wrong email or password\nPress ENTER to return...");
-            Console.ReadLine();
-            Console.Clear();
-            }
-        }
-        while (activeUser != null)
-        {
-          Console.WriteLine("---Welcome---");
-          try { Console.ReadLine();} catch{}
-          break;
-        }
-        break;
+        Console.Clear();
     }
-  }
+    catch { }
+    switch (menu)
+    {
+        case Menu.None:
+            {
+                if (activeUser == null)
+                {
+                    Console.WriteLine("---Welcome to health care system---");
+                    Console.WriteLine("1] Login");
+                    Console.WriteLine("2] Quit");
+
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            menu = Menu.Login;
+                            break;
+                        case "2":
+                            running = false;
+                            break;
+                    }
+                }
+                else
+                {
+                    menu = Menu.Main;
+                }
+            }
+            break;
+
+        case Menu.Login:
+            try
+            {
+                Console.Clear();
+            }
+            catch { }
+            Console.WriteLine("Enter Email: ");
+            string? email = Console.ReadLine();
+            try
+            {
+                Console.Clear();
+            }
+            catch { }
+
+            Console.WriteLine("Enter password: ");
+            string? password = Console.ReadLine();
+            try
+            {
+                Console.Clear();
+            }
+            catch { }
+
+            Debug.Assert(email != null);
+            Debug.Assert(password != null);
+
+            if (email == "" && password == "")
+            {
+                Console.WriteLine("Enter valid input");
+            }
+
+            foreach (User user in users)
+            {
+                if (user.TryLogin(email, password))
+                {
+                    activeUser = user;
+                    break;
+                }
+            }
+            menu = Menu.None;
+            break;
+
+        case Menu.Main:
+
+            try
+            {
+                Console.Clear();
+            }
+            catch { }
+            Debug.Assert(activeUser != null);
+            Console.WriteLine($"Welcome {activeUser.Email}");
+            Console.WriteLine("'Q' for quit and 'L' for log out");
+
+            switch (Console.ReadLine())
+            {
+                case "Q":
+                    running = false;
+                    break;
+                case "L":
+                    activeUser = null;
+                    menu = Menu.None;
+                    break;
+            }
+            break;
+    }
 }
