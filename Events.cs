8e3dd9cfb742368023250b399f8d68d335dfabcc
andymalm-ux <andreas.malmgren@student.nbi-handelsakademin.se>
2025-10-16 10:44:20 +0200

    added file Event.cs

---
+++ Events.cs
@@ -0,0 +1,11 @@
+namespace App;
+
+public class Events
+{
+    //List<Participants> participants = new List<Participants>();
+    //Date.Time lägga till tid för eventets start och stopp
+    //Participants, roller
+    //Title på event
+    //Beskrivning på event
+    //Plats för eventet
+}
